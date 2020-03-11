7322858c2085b04a31345aa64598c247b3a70354
hsisco <hysensisco@gmail.com>
2020-03-10 20:22:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const db = require('./data/hubs-model.js');
 
 const server = express();
 
 server.listen(4000, () => {
   console.log('listening on port 4000...')
 });
+
+// HTTP Method
+// URI : scheme://host_name:port/path?parameter_list
+//       http://www.google.com/some/document?with_params=value
+
+// server.get('/', (request, response) => {   <-- The Homies
+server.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+server.get('/hey', (req, res) => {
+  res.send('Hey there!');
+})
+
+server.get('/favicon.ico', (req, res) => { res.status(204) });
+
+server.get('/hubs', (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.status(200).json({ hubs });
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+})
